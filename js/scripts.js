756101c2d7dc02cdf5ac5df571605a944d36a9af
mattcaswell <matthewcaswell253@Gmail.com>
2017-02-01 09:55:46 -0800

    add back end logic to scripts with formula for pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function Pizza(toppings, size) {
 this.toppings = toppings;
 this.size = size;
 }
+
+Pizza.prototype.price = function() {
+  var toppingPrice = 1.50;
+  var sizPriceStep = 2.00;
+  var price = 0;
+
+  price += this.toppings.length * toppingPrice;
+  price += this.size * sizePriceStep;
+
+  return price;
+}
