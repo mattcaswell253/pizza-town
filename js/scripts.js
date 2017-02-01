e81acd0ad81aaf749b7163165866a9678ef5e91d
mattcaswell <matthewcaswell253@Gmail.com>
2017-02-01 13:02:51 -0800

    add functionality to website

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+  $("#order-form").submit(function(event) {
+    event.preventDefault();
+    var selectedToppings = [];
+    var selectedSize = $("select#size").val();
+
+    $("input[name=toppings]:checked").each(function() {
+       selectedToppings.push($(this).val());
+    });
+
+    var MyPizza = new Pizza(selectedToppings, selectedSize);
+
+$("span#total").html("$" + MyPizza.price());
+  });
+})
+
 function Pizza(toppings, size) {
 this.toppings = toppings;
 this.size = size;
@@ -5,8 +21,8 @@ this.size = size;
 
 Pizza.prototype.price = function() {
   var toppingPrice = 1.50;
-  var sizPriceStep = 2.00;
-  var price = 0;
+  var sizePriceStep = 2.00;
+  var price = 0.00;
 
   price += this.toppings.length * toppingPrice;
   price += this.size * sizePriceStep;
